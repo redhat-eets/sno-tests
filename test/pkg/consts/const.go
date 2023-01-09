53c56321757e9eb4fc9ed8843b3946446ef6e215
Sabina Aledort <saledort@redhat.com>
2023-01-09 11:12:04 +0200

    Add validation test to verify NVM Firmware
    Verifying the firmware version is needed since
    some required features are not implemented in
    old firmware versions.

--- test/pkg/consts/const.go
+++ test/pkg/consts/const.go
@@ -5,4 +5,6 @@ const (
 	TestNamespace = "sno-testing"
 	// WPCDeviceType is the type of the tested WPC NIC
 	WPCDeviceType = "E810-XXV-4T"
+	// ICEDriverFirmwareVerMinVersion is the minimal ICE driver version required for WPC NIC
+	ICEDriverFirmwareVerMinVersion = 4.01
 )
