cc16f2f0f2fbd67bcd1da89cc1599e7e0cbc7208
Sabina Aledort <saledort@redhat.com>
2023-01-19 11:58:51 +0200

    T-GM validation test: Fix firmware version variable
    The firmware version is associated to the card,
    unrelated to the driver.

--- test/pkg/consts/const.go
+++ test/pkg/consts/const.go
@@ -5,8 +5,8 @@ const (
 	TestNamespace = "sno-testing"
 	// WPCDeviceType is the type of the tested WPC NIC
 	WPCDeviceType = "E810-XXV-4T"
-	// ICEDriverFirmwareVerMinVersion is the minimal ICE driver version required for WPC NIC
-	ICEDriverFirmwareVerMinVersion = 4.01
+	// NVMFirmwareMinVersion is the minimal firmware version required for WPC NIC
+	NVMFirmwareMinVersion = 4.01
 	// DPLLLockedHOACQState is the required DPLL state -> 3: DPLL Locked Holdover Acquired
 	DPLLLockedHOACQState = "3"
 	// DPLLMaxAbsOffset is the maximum absolute DPLL offset in nanoseconds
